5c4a3b80f862790f441d9cacfa4a9f53a708c271
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2026-04-24 07:37:07 +0200

    Prevent division by 0 in tool outline.
    This could happen when creating a selection on pixelart, and the outline
    was zero-width.

--- libs/ui/opengl/KisOpenGLCanvasRenderer.cpp
+++ libs/ui/opengl/KisOpenGLCanvasRenderer.cpp
@@ -486,9 +486,11 @@ void KisOpenGLCanvasRenderer::paintToolOutline(const KisOptimizedBrushOutline &p
 
                     const qreal dot = KisAlgebra2D::dotProduct(miter, normal);
 
-                    if (KisAlgebra2D::norm((miter * halfWidth) / dot) < miterLimit) {
-                        c1 = p1 + ((miter * -halfWidth) / dot);
-                        c2 = p1 + ((miter * halfWidth) / dot);
+                    const qreal dotMulti = dot != 0? 1.0/dot: 0.0;
+
+                    if (KisAlgebra2D::norm((miter * halfWidth) * dotMulti) < miterLimit) {
+                        c1 = p1 + ((miter * -halfWidth) * dotMulti);
+                        c2 = p1 + ((miter * halfWidth) * dotMulti);
                     }
                 }
 
@@ -500,10 +502,11 @@ void KisOpenGLCanvasRenderer::paintToolOutline(const KisOptimizedBrushOutline &p
                         KisAlgebra2D::normalize(
                             normal + KisAlgebra2D::normalize(QPointF(-pNext.y(), pNext.x())));
                     const qreal dot = KisAlgebra2D::dotProduct(miter, normal);
+                    const qreal dotMulti = dot != 0? 1.0/dot: 0.0;
 
-                    if (KisAlgebra2D::norm((miter * halfWidth) / dot) < miterLimit) {
-                        c3 = p2 + ((miter * -halfWidth) / dot);
-                        c4 = p2 + (miter * halfWidth) / dot;
+                    if (KisAlgebra2D::norm((miter * halfWidth) * dotMulti) < miterLimit) {
+                        c3 = p2 + ((miter * -halfWidth) * dotMulti);
+                        c4 = p2 + (miter * halfWidth) * dotMulti;
                     }
                 }
 
